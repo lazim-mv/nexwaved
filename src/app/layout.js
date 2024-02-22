2db4cb0f64f255e19dfb729a5cf4a6007a1ddbe5
lazim mv <lazimlatheef@gmail.com>
2024-02-22 08:52:08 +0530

    v0.0.4 contactPage

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import SmoothScrolling from "./components/SmoothScroll/SmoothScrolling";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,9 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <SmoothScrolling>{children}</SmoothScrolling>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
